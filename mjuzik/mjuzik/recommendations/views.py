2519aa730775f73eeea93678dec9dbae0eab435e
DylanGuedes <djmgguedes@gmail.com>
2016-07-06 18:43:22 -0300

    adds new uniquearray plugin as a pip package'

--- mjuzik/mjuzik/recommendations/views.py
+++ mjuzik/mjuzik/recommendations/views.py
@@ -7,14 +7,16 @@ from django.contrib.auth.decorators import login_required
 from django.core.signals import request_finished
 from django.dispatch import receiver
 from django.db.models.signals import m2m_changed
-from mjuzik import load_plugin, get_plugins, load_plugin_from_name
+# from mjuzik import load_plugin, get_plugins, load_plugin_from_name
+from mjuzik.uniquearray import run as transform_into_unique
 
 @login_required(login_url='/login')
 def index(request):
-    plugin = load_plugin_from_name("uniquearray")
+    # plugin = load_plugin_from_name("uniquearray")
     genres_ids = request.user.profile.following_genres.all().values_list('id', flat=True)
     recommendations = Recommendation.objects.filter(genres__in=genres_ids).order_by('-created_at')
-    recommendations = plugin.run(recommendations)
+    # recommendations = plugin.run(recommendations)
+    recommendations = transform_into_unique(recommendations)
     context = {'recommendations':recommendations}
     return render(request, 'recommendations/index.html', context)
 
---
+++ mjuzik/mjuzik/uniquearray/README.rst
@@ -0,0 +1,18 @@
+=====
+UniqueArray
+=====
+
+UniqueArray is a simple plugin that makes a non-unique collection into an unique one. The order of the items still the same.
+
+Quick start
+-----------
+
+1. Add "uniquearray" to your INSTALLED_APPS setting like this::
+
+    INSTALLED_APPS = [
+        ...
+        'uniquearray',
+    ]
+
+2. Import it, like this::
+    from uniquearray import run as unique_transform
